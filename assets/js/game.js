f3b54c3f7e3561fd6a48c1846991d470ae0232e4
trm621 <t.maher621@gmail.com>
2021-10-09 18:38:20 -0400

    fixed shop to accept integers

--- assets/js/game.js
+++ assets/js/game.js
@@ -126,22 +126,21 @@ for (var i = 0; i < enemyInfo.length; i++) {
 
 var shop = function() {
    var shopOptionPrompt = window.prompt(
-       "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+       "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 to LEAVE."
    );
 
+shopOptionPrompt = parseInt(shopOptionPrompt);
+
 switch (shopOptionPrompt) {
-    case "REFILL":
-    case "refill":
+    case 1:
         playerInfo.refillHealth();
         break;
 
-    case "UPGRADE":
-    case "upgrade":
+    case 2:
         playerInfo.upgradeAttack();
         break;
 
-    case "LEAVE":
-    case "leave":
+    case 3:
         window.alert("Leaving the store.");
         break;
     default:
