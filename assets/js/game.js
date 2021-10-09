9d407956c5fecb3888f177925dcb495d4f0df833
trm621 <t.maher621@gmail.com>
2021-10-09 18:09:39 -0400

    fixed bugs with input in fight section

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,23 +4,38 @@ var randomNumber = function(min, max) {
     return value;
   };
 
-var fight = function(enemy) {
-    while(playerInfo.health > 0 && enemy.health > 0)  {
-    // Alert players that they are starting the round
-        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-    if (promptFight === "skip" || promptFight === "SKIP") {
-    // confirm player wants to skip
-    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-    
-    if (confirmSkip) {
-        window.alert(playerInfo.name + " has decided to skip this fight! Goodbye!");
-        playerInfo.money = Math.max(0, playerInfo.money - 10);
-        console.log("playerInfo.money", playerInfo.money);
-        break;
+var fightOrSkip = function() {
+    var promptFight = prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.");
+      
+      if (promptFight === "" || promptFight === null) {
+          window.alert("You need to provide a valid answer! Please try again.");
+          return fightOrSkip();
+      }
+
+      promptFight = promptFight.toLowerCase();
+
+      if (promptFight === "skip") {
+          var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+      
+      if (confirmSkip) {
+          window.alert(playerInfo.name + " has decided to skip the fight. Goodbye!");
+          playerInfo.money = Math.max(0, playerInfo.money - 10);
+      return true;
     }
+      else {
+      return false;
+    }
+  }
 }
-    
-var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+
+var fight = function(enemy) {
+
+    while(playerInfo.health > 0 && enemy.health > 0)  {
+      if (fightOrSkip()) {
+          break;
+      }
+      
+    var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
   enemy.health = Math.max(0, enemy.health - damage);
   console.log(
