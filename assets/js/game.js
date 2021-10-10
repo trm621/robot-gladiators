a08ffdd44fc87601ea4194499c10f28dfdc9e8ee
trm621 <t.maher621@gmail.com>
2021-10-09 20:21:20 -0400

    added high score storage

--- assets/js/game.js
+++ assets/js/game.js
@@ -128,6 +128,7 @@ for (var i = 0; i < enemyInfo.length; i++) {
         }
     }
 }
+
     else {
         window.alert("You have lost your robot in battle! Game Over!");
         break;
@@ -140,18 +141,25 @@ for (var i = 0; i < enemyInfo.length; i++) {
   var endGame = function() {
         window.alert("The game has now ended. Let's see how you did!");
         
-        if (playerInfo.health > 0) {
-          window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
-        }
-        else {
-          window.alert("You've lost your robot in battle.");
-        }
-    
-  var playAgainConfirm = window.confirm("Would you like to play again?");
+        var highScore = localStorage.getItem("highscore") 
+          if (highScore === null) {
+            highScore = 0;
+          }
+          if (playerInfo.money > highScore) {
+            localStorage.setItem("highscore", playerInfo.money);
+            localStorage.setItem("name", playerName);
 
-  if (playAgainConfirm) {
-        startGame();
-  } else {
+            alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+          }
+          else{
+            alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+          }
+
+        var playAgainConfirm = window.confirm("Would you like to play again?");
+
+        if (playAgainConfirm) {
+          startGame();
+        } else {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
   }
 };
